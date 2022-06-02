84530f8edafafe565c2df986eed6fa1d4b36818e
Bill <Bill@qq.com>
2022-06-02 08:43:55 +0800

    add springmvc module

---
+++ spring-springmvc/src/test/java/com/bh/springspringmvc/SpringSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh.springspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
